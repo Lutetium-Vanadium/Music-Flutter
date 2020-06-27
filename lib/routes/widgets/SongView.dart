5947ca7b5da12e1789a420fb0b049196b51e8b84
Lutetium-Vanadium <luv.s7000@gmail.com>
2020-06-27 12:12:05 +0530

    Extracted SongView to build a list of Song widgets

--- lib/routes/widgets/SongView.dart
+++ lib/routes/widgets/SongView.dart
@@ -1,12 +1,10 @@
 import "dart:io";
 import "package:flutter/material.dart";
-import "package:focused_menu/modals.dart";
-import "package:focused_menu/focused_menu.dart";
 
-import "package:Music/CustomSplashFactory.dart";
-import "package:Music/helpers/formatLength.dart";
 import "package:Music/constants.dart";
 import "package:Music/models/models.dart";
+
+import "./Song.dart";
 
 class SongView extends StatelessWidget {
   final List<SongMetadata> songs;
@@ -74,155 +72,49 @@ class SongView extends StatelessWidget {
           }
 
           var song = songs[index];
+          return Song(
+            iconData: iconData,
+            song: song,
+            onClick: () {
+              if (onClick != null) {
+                onClick(song, index);
+              }
+            },
+            showFocusedMenuItems: showFocusedMenuItems,
+            image: isNetwork
+                ? Image.network(
+                    song.thumbnail,
+                    fit: BoxFit.scaleDown,
+                    width: 4 * rem,
+                    height: 4 * rem,
+                    frameBuilder: (ctx, widget, frame, synchronouslyLoaded) {
+                      if (synchronouslyLoaded) {
+                        return widget;
+                      }
 
-          return Container(
-            margin: EdgeInsets.symmetric(
-              vertical: 0.6 * rem,
-              horizontal: 1.2 * rem,
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(rem / 2),
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-            child: Material(
-              color: Colors.transparent,
-              child: !showFocusedMenuItems
-                  ? _buildSongDetails(context, song, index)
-                  : FocusedMenuHolder(
-                      menuItems: [
-                        FocusedMenuItem(
-                          onPressed: () => onClick(song, index),
-                          title: Text("Play"),
-                          trailingIcon: Icon(Icons.play_arrow),
-                          backgroundColor: Colors.transparent,
+                      return AnimatedCrossFade(
+                        firstChild: Image.asset(
+                          "$imgs/music_symbol.png",
+                          fit: BoxFit.scaleDown,
+                          width: 4 * rem,
+                          height: 4 * rem,
                         ),
-                        FocusedMenuItem(
-                          onPressed: () {},
-                          title: Text("Like"),
-                          trailingIcon: Icon(Icons.favorite_border),
-                          backgroundColor: Colors.transparent,
-                        ),
-                        FocusedMenuItem(
-                          onPressed: () {},
-                          title: Text("Delete",
-                              style: TextStyle(color: Colors.red)),
-                          trailingIcon: Icon(Icons.delete, color: Colors.red),
-                          backgroundColor: Colors.transparent,
-                        ),
-                      ],
-                      child: _buildSongDetails(context, song, index),
-                      onPressed: () {},
-                      animateMenuItems: false,
-                      blurSize: 5,
-                      menuBoxDecoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(6),
-                        color: Theme.of(context).canvasColor.withOpacity(0.69),
-                      ),
-                      blurBackgroundColor: Colors.transparent,
-                      duration: Duration(milliseconds: 300),
-                      menuWidth: 150,
-                    ),
-            ),
+                        secondChild: widget,
+                        crossFadeState: frame == null
+                            ? CrossFadeState.showFirst
+                            : CrossFadeState.showSecond,
+                        duration: Duration(milliseconds: 400),
+                      );
+                    },
+                  )
+                : Image.file(
+                    File(song.thumbnail),
+                    fit: BoxFit.scaleDown,
+                    width: 4 * rem,
+                    height: 4 * rem,
+                  ),
           );
         },
-      ),
-    );
-  }
-
-  Widget _buildSongDetails(BuildContext context, SongData song, int index) {
-    return InkWell(
-      onTap: () {
-        if (onClick != null) {
-          onClick(song, index);
-        }
-      },
-      splashFactory: CustomSplashFactory(),
-      splashColor: Theme.of(context).primaryColor,
-      borderRadius: BorderRadius.circular(rem / 2),
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          vertical: 0.6 * rem,
-          horizontal: 1.2 * rem,
-        ),
-        child: Flex(
-          direction: Axis.horizontal,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(0.6 * rem),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(0.4 * rem),
-                child: isNetwork
-                    ? Image.network(
-                        song.thumbnail,
-                        fit: BoxFit.scaleDown,
-                        width: 4 * rem,
-                        height: 4 * rem,
-                        frameBuilder:
-                            (ctx, widget, frame, synchronouslyLoaded) {
-                          if (synchronouslyLoaded) {
-                            return widget;
-                          }
-
-                          return AnimatedCrossFade(
-                            firstChild: Image.asset(
-                              "$imgs/music_symbol.png",
-                              fit: BoxFit.scaleDown,
-                              width: 4 * rem,
-                              height: 4 * rem,
-                            ),
-                            secondChild: widget,
-                            crossFadeState: frame == null
-                                ? CrossFadeState.showFirst
-                                : CrossFadeState.showSecond,
-                            duration: Duration(milliseconds: 400),
-                          );
-                        },
-                      )
-                    : Image.file(
-                        File(song.thumbnail),
-                        fit: BoxFit.scaleDown,
-                        width: 4 * rem,
-                        height: 4 * rem,
-                      ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: rem, right: 1.5 * rem),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      song.title,
-                      style: Theme.of(context).textTheme.bodyText1,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 0.5 * rem, left: 0.8 * rem),
-                      child: Text(
-                        song.artist,
-                        style: Theme.of(context).textTheme.subtitle2,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Text(formatLength(song.length)),
-            Padding(
-              padding: iconData == null
-                  ? EdgeInsets.all(0)
-                  : EdgeInsets.only(left: 13, right: 3),
-              child: Icon(
-                iconData,
-                size: 1.5 * rem,
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
