ba3a907d87c45e43077f4637bb417b653a8907ff
Lutetium-Vanadium <luv.s7000@gmail.com>
2020-12-23 12:41:01 +0530

    Fix failing to download

--- lib/helpers/getYoutubeDetails.dart
+++ lib/helpers/getYoutubeDetails.dart
@@ -14,10 +14,8 @@ Future<List<YoutubeDetails>> getYoutubeDetails(
     var yt = YoutubeExplode();
     var i = 0;
 
-    var res = await yt.search
-        .getVideosAsync(query)
-        .takeWhile((_) => i++ < 5)
-        .toList();
+    var res =
+        await yt.search.getVideos(query).takeWhile((_) => i++ < 5).toList();
 
     yt.close();
     return res
